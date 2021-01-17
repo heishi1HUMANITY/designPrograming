733a5a7aa32fc0d0b3fb0f562c6eb8b41cfab06a
heishi1HUMANITY <i.wanna.make.this.main@gmail.com>
2021-01-17 13:51:58 +0900

    initial commit

---
+++ main.ts
@@ -0,0 +1,49 @@
+const faceapi = require('face-api.js');
+const canvas = require('canvas');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const multer = require('multer');
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req: any, file: any, cb: any) => {
+      cb(null, './uploads/');
+    },
+    filename: (req: any, file: any, cb: any) => {
+      cb(null, 'tmp.jpg');
+    }
+  })
+});
+const app = express();
+const getParam = async (): Promise<any> => {
+  const c = canvas.createCanvas(320, 240);
+  const ctx = c.getContext('2d');
+  const img = await canvas.loadImage(path.join(__dirname, 'uploads', 'tmp.jpg'));
+  ctx.drawImage(img, 0, 0, 320, 240);
+  const d = await faceapi.detectSingleFace(c).withFaceExpressions();
+  return d;
+};
+const max = (obj: object): [string, number] => {
+  let max: [string, number] = ['', -Infinity]
+  for (const [key, value] of Object.entries(obj)) {
+    if (max[1] <= value) max = [key, value];
+  }
+  console.log(max);
+  return max;
+}
+
+app.post('/api/dp/', upload.any(), async (req: any, res: any): Promise<void> => {
+  const p = await getParam();
+  if (p !== null) res.send(max(p.expressions).join(','));
+  else res.end();
+});
+
+app.listen(8080, () => console.log('server is working at localhost:8080'));
+
+(async () => {
+  const { Canvas, Image, ImageData } = canvas;
+  faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
+  await faceapi.nets.ssdMobilenetv1.loadFromDisk(path.join(__dirname, 'weights'));
+  await faceapi.nets.faceExpressionNet.loadFromDisk(path.join(__dirname, 'weights'));
+  console.log('faceapi is ready');
+})();
